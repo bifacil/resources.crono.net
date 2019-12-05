9de30d6286a1fbc3d386027e95f11a0116a5a6ba
Pau <pau.urquizu@crono.net>
2019-12-05 15:06:36 +0100

    md-magic: Se elimina ficharo innecesario y se incluye ejemplo

--- md-magic/script.js
+++ md-magic/script.js
@@ -26,9 +26,36 @@
     script.setAttribute('src', 'https://bifacil.github.io/resources.crono.net/md-magic/showdown.js')
     document.head.appendChild(script)
 
-    var script2 = document.createElement('script')
-    script2.setAttribute('src', 'https://bifacil.github.io/resources.crono.net/md-magic/converter.js')
-    document.head.appendChild(script2)
+
+    script.addEventListener('load', () => {
+
+        var markdown = document.querySelector('noscript').innerText
+        var converter = new showdown.Converter({
+            emoji: true,
+            underline: true,
+        })
+        converter.setFlavor('github')
+    
+        converter.addExtension(function () {
+            return [{
+                type: 'output',
+                regex: /<a\shref[^>]+>/g,
+                replace : function (text) {
+                    var url = text.match(/"(.*?)"/)[1]
+                    if(url.includes(window.location.hostname) || url[0] == '/' || url[0] == '.' || url[0] == '#'){
+                        return text
+                    }
+                    return '<a href="' + url + '" target="_blank">'
+                }
+            }]
+        }, 'externalLink')
+    
+    
+        var html = "<div id='md'>" + converter.makeHtml(markdown) +'</div>'
+        document.body.innerHTML = html
+        document.title = document.title || document.body.firstElementChild.innerText.trim()
+    
+    })
 
     // Handle hash linking
     setTimeout(function() {
